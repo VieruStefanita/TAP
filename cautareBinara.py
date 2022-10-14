c8647f5013adf3f8481db5151e7f2b2089f196a9
VieruStefanita <115227847+VieruStefanita@users.noreply.github.com>
2022-10-14 15:53:42 +0300

    Add files via upload

--- cautareBinara.py
+++ cautareBinara.py
@@ -5,8 +5,25 @@ def citeste():
     for i in range(0,n):
         element=input("introdu nr: ")
         lista.append(element)
-    
-def binarySearch(lista, m, low, high):
+
+def insertionSort(lista):
+
+    for step in range(1, len(lista)):
+        key = lista[step]
+        j = step - 1
+        
+           
+        while j >= 0 and key < lista[j]:
+            lista[j + 1] = lista[j]
+            j = j - 1
+        
+        # Place key at after the element just smaller than it.
+        lista[j + 1] = key
+
+
+
+
+def binarySearch_iterative(lista, m, low, high):
 
    while low <= high:
 
@@ -21,11 +38,13 @@ def binarySearch(lista, m, low, high):
         else:
             high = mid - 1
 
-    return -1
+        return -1
        
-    
+ 
 
 citeste()
+insertionSort(lista)
+print(lista)
 m=input("Ce numar cauti?")
-rez=binarySearch(lista, m, 0, len(lista)-1)
+rez=binarySearch_iterative(lista, m, 0, len(lista)-1)
 print("Elementul poate fi gasit la pozitia: ",rez)
